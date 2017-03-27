2d970724bc9b7eef93bb1b7cad15a5e7ce1a2e6c
AnnaRokalo <rokalo.a.s@gmail.com>
2017-03-27 18:47:33 +0300

    Add app files.

---
+++ src/reducers/reducer__books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Harry Potter', pages: 390},
+        {title: 'The lord of the rings', pages: 800},
+        {title: 'Sherlock Holmes', pages: 530},
+        {title: 'Hamlet', pages: 120}
+    ];
+}
